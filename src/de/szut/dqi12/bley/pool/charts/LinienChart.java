f4134af5e318a0a80ad5192b59d605be33e4cee4
Robin Bley <robin-bley@hotmail.de>
2015-03-04 22:05:35 +0100

    LinienChart

--- src/de/szut/dqi12/bley/pool/charts/LinienChart.java
+++ src/de/szut/dqi12/bley/pool/charts/LinienChart.java
@@ -6,7 +6,13 @@
 package de.szut.dqi12.bley.pool.charts;
 
 import info.monitorenter.gui.chart.Chart2D;
+import info.monitorenter.gui.chart.ITrace2D;
+import info.monitorenter.gui.chart.traces.Trace2DSimple;
+import info.monitorenter.gui.chart.views.ChartPanel;
+import java.awt.Color;
 import java.util.ArrayList;
+import javax.swing.BorderFactory;
+import javax.swing.border.TitledBorder;
 
 /**
  *
@@ -14,9 +20,40 @@ import java.util.ArrayList;
  */
 public class LinienChart implements Chart {
 
+    private int[] range;
+    private Chart2D chart;
+    private ITrace2D trace;
+
+    public LinienChart(String title) {
+        Chart2D chart = new Chart2D();
+
+        // Create an ITrace:
+        // Note that dynamic charts need limited amount of values!!!
+        // ITrace2D trace = new Trace2DLtd(200);
+        ITrace2D trace = new Trace2DSimple();
+        // Add the trace to the chart:
+        chart.addTrace(trace);
+        trace.setColor(Color.RED);
+
+
+        ChartPanel chartPanel = new ChartPanel(chart);
+        // add a titlede border:
+
+        chartPanel.setBorder(BorderFactory.createTitledBorder(BorderFactory
+                .createLineBorder(Color.BLACK), title, TitledBorder.CENTER, TitledBorder.CENTER));
+
+    }
+
     @Override
     public Chart2D generateChart(ArrayList<Double[]> data) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (double i = range[0]; i < range[1]; i++) {
+            if (data.size() < i) {
+                break;
+            }
+            trace.addPoint(data.get((int) i)[0], data.get((int) i)[1]);
+
+        }
+        return chart;
     }
-    
+
 }
--- src/main.java
+++ src/main.java
@@ -4,19 +4,29 @@ import info.monitorenter.gui.chart.Chart2D;
 import info.monitorenter.gui.chart.IAxis;
 import info.monitorenter.gui.chart.IPointHighlighter;
 import info.monitorenter.gui.chart.ITrace2D;
+import info.monitorenter.gui.chart.ZoomableChart;
+import info.monitorenter.gui.chart.axis.AAxis;
+import info.monitorenter.gui.chart.axis.AxisLinear;
 import info.monitorenter.gui.chart.labelformatters.LabelFormatterDate;
 import info.monitorenter.gui.chart.pointhighlighters.PointHighlighterConfigurable;
 import info.monitorenter.gui.chart.pointpainters.PointPainterDisc;
 import info.monitorenter.gui.chart.rangepolicies.RangePolicyFixedViewport;
 import info.monitorenter.gui.chart.traces.Trace2DSimple;
 import info.monitorenter.gui.chart.traces.painters.TracePainterVerticalBar;
+import info.monitorenter.gui.chart.views.ChartPanel;
 import info.monitorenter.util.Range;
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
 import java.text.SimpleDateFormat;
 import java.util.Random;
 import java.util.Set;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.UIManager;
+import javax.swing.border.TitledBorder;
+import vccc.MultiAxisZoomTest;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -30,64 +40,8 @@ import javax.swing.UIManager;
 public class main {
 
     public static void main(String[] args) {
-        Chart2D chart = new Chart2D();
-    
-    // Obtain the basic default axes: 
-    IAxis axisX = chart.getAxisX();
-    IAxis axisY = chart.getAxisY();
-    
-    // Feature: Grids:
-    chart.setGridColor(Color.LIGHT_GRAY);
-    axisX.setPaintGrid(true);
-    axisY.setPaintGrid(true);
-
-    // Create an ITrace:
-    ITrace2D trace = new Trace2DSimple();
-    // Add the trace to the chart:
-    chart.addTrace(trace);
-    
-    // Feature: trace painters: You are also able to specify multiple ones!
-    trace.setTracePainter(new TracePainterVerticalBar(4, chart));
-    
-    // Feature: trace color. 
-    trace.setColor(Color.ORANGE);
-    
-    // Feature: Axis title font. 
-    Font titleFont = UIManager.getDefaults().getFont("Label.font").deriveFont(14f).deriveFont(
-        Font.BOLD);
-    IAxis.AxisTitle axisTitle = axisY.getAxisTitle();
-    axisTitle.setTitleFont(titleFont);
-    
-    // Feature: axis title.
-//    axisTitle.setTitle("Hoppelhase");
-    
-    // Feature: axis formatter.
-    axisY.setFormatter(new LabelFormatterDate(new SimpleDateFormat()));
-    
-    // Feature: axis title (again).
-//    axisTitle = axisX.getAxisTitle();
-//    axisTitle.setTitle("Emil");
-//    axisTitle.setTitleFont(titleFont);
-    
-    // Feature: range policy for axis. 
-    axisX.setRangePolicy(new RangePolicyFixedViewport(new Range(0, 220)));
-    
-    // Feature: turn on tool tips (recommended for use in static mode only): 
-    chart.setToolTipType(Chart2D.ToolTipType.VALUE_SNAP_TO_TRACEPOINTS);
-    
-    // Feature: turn on highlighting: Two steps enable it on the chart and set a highlighter for the trace: 
-    Set<IPointHighlighter<?>> highlighters = trace.getPointHighlighters();
-    highlighters.clear();
-    trace.addPointHighlighter(new PointHighlighterConfigurable(new PointPainterDisc(20), true));
-    chart.enablePointHighlighting(true);
-    chart.setBackground(Color.cyan);
-    // Add all points, as it is static:
-    double high = System.currentTimeMillis();
-    for (double i = 0; i < 20; i++) {
-      trace.addPoint(i * 10, high);
-      high += 1000 * 50;
-
-    }
+          
+    // Make it visible:
         new Gui().setGraph(chart);
     }
 
